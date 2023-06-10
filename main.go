a6717aa1afb60fd8f23b26c968b1476180913284
parsee1053 <s.naiki.parsee1053@gmail.com>
2023-06-10 19:36:50 +0900

    ポート番号を変更できるようにする

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"net"
 	"net/http"
 	"os"
 
@@ -11,15 +12,25 @@ import (
 )
 
 func main() {
-	if err := run(context.Background()); err != nil {
+	if len(os.Args) != 2 {
+		log.Printf("need port number\n")
+		os.Exit(1)
+	}
+	p := os.Args[1]
+	l, err := net.Listen("tcp", ":"+p)
+	if err != nil {
+		log.Fatalf("failed to listen port %s: %v", p, err)
+	}
+	if err := run(context.Background(), l); err != nil {
 		fmt.Printf("failed to terminate server: %v", err)
 		os.Exit(1)
 	}
 }
 
-func run(ctx context.Context) error {
+func run(ctx context.Context, l net.Listener) error {
 	s := &http.Server{
-		Addr: ":18080",
+		// 引数で受け取ったnet.Listenerを利用するので、
+		// Addrフィールドは指定しない
 		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
 		}),
@@ -27,9 +38,11 @@ func run(ctx context.Context) error {
 	eg, ctx := errgroup.WithContext(ctx)
 	// 別ゴルーチンでHTTPサーバーを起動する
 	eg.Go(func() error {
-		// http.ErrServerClosed は
-		// http.Server.Shutdown() が正常に終了したことを示すので異常ではない。
-		if err := s.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+		// ListenAndServeメソッドではなく、Serveメソッドに変更する
+		if err := s.Serve(l); err != nil &&
+			// http.ErrServerClosed は
+			// http.Server.Shutdown() が正常に終了したことを示すので異常ではない。
+			err != http.ErrServerClosed {
 			log.Printf("failed to close: %+v", err)
 			return err
 		}
