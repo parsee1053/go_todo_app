0ec28e363b86ccb1efe9bbc8040a19ca01c52171
parsee1053 <s.naiki.parsee1053@gmail.com>
2023-06-17 14:10:31 +0900

    entity.Task型の定義と永続化方法の仮実装

---
+++ store/store.go
@@ -0,0 +1,25 @@
+package store
+
+import "github.com/parsee1053/go_todo_app/entity"
+
+type TaskStore struct {
+	// 動作確認用の仮実装なのであえてexportしている。
+	LastID entity.TaskID
+	Tasks  map[entity.TaskID]*entity.Task
+}
+
+func (ts *TaskStore) Add(t *entity.Task) (entity.TaskID, error) {
+	ts.LastID++
+	t.ID = ts.LastID
+	ts.Tasks[t.ID] = t
+	return t.ID, nil
+}
+
+// Allはソート済みのタスク一覧を返す
+func (ts *TaskStore) All() entity.Tasks {
+	tasks := make([]*entity.Task, len(ts.Tasks))
+	for i, t := range ts.Tasks {
+		tasks[i-1] = t
+	}
+	return tasks
+}
