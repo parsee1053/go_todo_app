1f9546bc3bc4e312591cb037099866eec37b1121
parsee1053 <s.naiki.parsee1053@gmail.com>
2023-07-02 19:05:25 +0900

    全てのタスクを取得するメソッドの実装

---
+++ store/task.go
@@ -0,0 +1,20 @@
+package store
+
+import (
+	"context"
+
+	"github.com/parsee1053/go_todo_app/entity"
+)
+
+func (r *Repository) ListTasks(
+	ctx context.Context, db Queryer,
+) (entity.Tasks, error) {
+	tasks := entity.Tasks{}
+	sql := `SELECT
+			id, title, status, created, modified
+		FROM task;`
+	if err := db.SelectContext(ctx, &tasks, sql); err != nil {
+		return nil, err
+	}
+	return tasks, nil
+}
