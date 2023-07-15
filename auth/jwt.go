146c55ecd97e4ea0c0b0fc52266578fdaeb7396d
parsee1053 <s.naiki.parsee1053@gmail.com>
2023-07-15 23:47:17 +0900

    JWTを発行するメソッドの実装

--- auth/jwt.go
+++ auth/jwt.go
@@ -4,10 +4,19 @@ import (
 	"context"
 	_ "embed"
 	"fmt"
+	"time"
 
+	"github.com/google/uuid"
+	"github.com/lestrrat-go/jwx/v2/jwa"
 	"github.com/lestrrat-go/jwx/v2/jwk"
+	"github.com/lestrrat-go/jwx/v2/jwt"
 	"github.com/parsee1053/go_todo_app/clock"
 	"github.com/parsee1053/go_todo_app/entity"
+)
+
+const (
+	RoleKey     = "role"
+	UserNameKey = "user_name"
 )
 
 //go:embed cert/secret.pem
@@ -50,4 +59,28 @@ func parse(rawKey []byte) (jwk.Key, error) {
 		return nil, err
 	}
 	return key, nil
+}
+
+func (j *JWTer) GenerateToken(ctx context.Context, u entity.User) ([]byte, error) {
+	tok, err := jwt.NewBuilder().
+		JwtID(uuid.New().String()).
+		Issuer(`github.com/parsee1053/go_todo_app`).
+		Subject("access_token").
+		IssuedAt(j.Clocker.Now()).
+		Expiration(j.Clocker.Now().Add(30*time.Minute)).
+		Claim(RoleKey, u.Role).
+		Claim(UserNameKey, u.Name).
+		Build()
+	if err != nil {
+		return nil, fmt.Errorf("GenerateToken: failed to build token: %w", err)
+	}
+	if err := j.Store.Save(ctx, tok.JwtID(), u.ID); err != nil {
+		return nil, err
+	}
+
+	signed, err := jwt.Sign(tok, jwt.WithKey(jwa.RS256, j.PrivateKey))
+	if err != nil {
+		return nil, err
+	}
+	return signed, nil
 }
