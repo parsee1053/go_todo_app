c2f6706b7a6e4e05cdc6044d48753f5b3652bd34
parsee1053 <s.naiki.parsee1053@gmail.com>
2023-07-17 20:50:35 +0900

    タスクを追加するときはユーザー情報をタスクに残す

--- store/task.go
+++ store/task.go
@@ -25,10 +25,10 @@ func (r *Repository) AddTask(
 	t.Created = r.Clocker.Now()
 	t.Modified = r.Clocker.Now()
 	sql := `INSERT INTO task
-		(title, status, created, modified)
-	VALUES (?, ?, ?, ?)`
+		(user_id, title, status, created, modified)
+	VALUES (?, ?, ?, ?, ?)`
 	result, err := db.ExecContext(
-		ctx, sql, t.Title, t.Status, t.Created, t.Modified,
+		ctx, sql, t.UserID, t.Title, t.Status, t.Created, t.Modified,
 	)
 	if err != nil {
 		return err
